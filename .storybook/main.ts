761ae68b9b2faeb971efb083b4cc19c610323844
HEgshiglen <160819674+HEgshiglen@users.noreply.github.com>
2024-05-07 22:57:43 +0800

    fix

---
+++ .storybook/main.ts
@@ -0,0 +1,27 @@
+import type { StorybookConfig } from "@storybook/react-webpack5";
+
+const config: StorybookConfig = {
+  stories: [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+    "@storybook/addon-mdx-gfm",
+    "@storybook/addon-webpack5-compiler-swc",
+    "@chromatic-com/storybook"
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {
+      builder: {},
+    },
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
--- button_comp/src/components/Badge/Badge.tsx
+++ button_comp/src/components/Badge/Badge.tsx
@@ -2,17 +2,29 @@ import React from "react";
 import styles from "./Badge.module.css";
 
 export interface BadgeProps {
-    label: string;
-    constrast: 'light' | 'dark';
-    color: 'primary' | 'secondary' | 'warning' | 'destructive' | 'success' | 'orchid';
-    size: 'small' | 'medium' | 'large' | 'xl';
-    radius: 'default' | 'curved';
+  label: string;
+  constrast: "light" | "dark";
+  color:
+    | "primary"
+    | "secondary"
+    | "warning"
+    | "destructive"
+    | "success"
+    | "orchid";
+  size: "small" | "medium" | "large" | "xl";
+  radius: "default" | "curved";
 }
 
 const Badge = (props: BadgeProps) => {
-    const {label, constrast, color, size, radius} = props;
+  const { label, constrast, color, size, radius } = props;
 
-    return <div className={`${styles['badge-' + constrast + '-' + color]} ${styles['badge-' + size]} ${styles['badge-' + radius]}`}>{label}</div>;
+  return (
+    <div
+      className={`${styles["badge-" + constrast + "-" + color]} ${styles["badge-" + size]} ${styles["badge-" + radius]}`}
+    >
+      {label}
+    </div>
+  );
 };
 
 export default Badge;
