1d67cabaa40ce8286d5a5ba311bef538a0423d35
HEgshiglen <160819674+HEgshiglen@users.noreply.github.com>
2024-05-08 14:08:49 +0800

    fix

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,12 @@
 import type { StorybookConfig } from "@storybook/react-webpack5";
 
 const config: StorybookConfig = {
-  stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
+  // stories: [
+  //   "../stories/**/*.mdx",
+  //   "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  //   "../button_comp/src/components/**/*.stories.@(js|jsx)",
+  // ],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
@@ -12,7 +14,7 @@ const config: StorybookConfig = {
     "@storybook/addon-interactions",
     "@storybook/addon-mdx-gfm",
     "@storybook/addon-webpack5-compiler-swc",
-    "@chromatic-com/storybook"
+    "@chromatic-com/storybook",
   ],
   framework: {
     name: "@storybook/react-webpack5",
